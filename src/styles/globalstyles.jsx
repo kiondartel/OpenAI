e2ff7656527b11e1f3ceeea630208f1588292c54
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-28 19:57:17 -0300

    GlobalStyles

---
+++ src/styles/globalstyles.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+ * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ }
+
+ html {
+  font-size: medium;
+  scroll-behavior: smooth;
+  font-family: "Nunito",sans-serif;;
+ }
