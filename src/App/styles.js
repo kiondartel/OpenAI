54a7562883d3299c7e5436b5a5a4ec26509899d1
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-28 20:46:16 -0300

    App Styles

--- src/App/styles.js
+++ src/App/styles.js
@@ -1,5 +1,92 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  @-webkit-keyframes scale-up-center {
+    0% {
+      -webkit-transform: scale(0.5);
+      transform: scale(0.5);
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+  }
 
-export const Gradient = styled.div``;
+  @keyframes scale-up-center {
+    0% {
+      -webkit-transform: scale(0.5);
+      transform: scale(0.5);
+    }
+    100% {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+    }
+  }
+
+  @media screen and (max-width: 700px) {
+    .section__padding {
+      padding: 4rem;
+    }
+
+    .section__margin {
+      margin: 4rem;
+    }
+  }
+
+  @media screen and (max-width: 550px) {
+    .section__padding {
+      padding: 4rem 2rem;
+    }
+
+    .section__margin {
+      margin: 4rem 2rem;
+    }
+  }
+`;
+
+export const Gradient = styled.div`
+  background: -moz-radial-gradient(
+    circle at 3% 25%,
+    rgba(0, 40, 83, 1) 0%,
+    rgba(4, 12, 24, 1) 25%
+  );
+
+  background: -webkit-radial-gradient(
+    circle at 3% 25%,
+    rgba(0, 40, 83, 1) 0%,
+    rgba(4, 12, 24, 1) 25%
+  );
+
+  background: -o-radial-gradient(
+    circle at 3% 25%,
+    rgba(0, 40, 83, 1) 0%,
+    rgba(4, 12, 24, 1) 25%
+  );
+
+  background: -ms-radial-gradient(
+    circle at 3% 25%,
+    rgba(0, 40, 83, 1) 0%,
+    rgba(4, 12, 24, 1) 25%
+  );
+
+  background: radial-gradient(
+    circle at 3% 25%,
+    rgba(0, 40, 83, 1) 0%,
+    rgba(4, 12, 24, 1) 25%
+  );
+`;
+
+export const GradientText = styled.div`
+  background: linear-gradient(89.97deg, #ae67fa 1.84%, #f49867 102.67%);
+  background-clip: text;
+  -webkit-background-clop: text;
+  -webkit-text-fill-color: transparent;
+`;
+
+export const SectionPadding = styled.section`
+  padding: 4rem 6rem;
+`;
+
+export const SectionMargin = styled.section`
+  margin: 4rem 6rem;
+`;
