4dee89430190d06fdd1de3da550f2cc368f49dbc
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-28 20:16:37 -0300

    Containers

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from "./Blog/index";
+export { default as Feature } from "./Feature/index";
+export { default as Footer } from "./Footer/index";
+export { default as Possibility } from "./Possibility/index";
+export { default as WhatGp3 } from "./WhatGp3/index";
+export { default as Header } from "./Header/index";
