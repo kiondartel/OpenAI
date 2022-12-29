40d6d03e647945114b83a6f9fb65337c6ba6d9dd
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-29 16:44:52 -0300

    Brand Images

--- src/components/Brand/index.jsx
+++ src/components/Brand/index.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import * as Styled from "./styles";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 function Brand() {
-  return <div />;
+  return (
+    <Styled.Container>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </Styled.Container>
+  );
 }
 
 export default Brand;
--- src/components/Brand/styles.js
+++ src/components/Brand/styles.js
@@ -1,3 +1,20 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 4rem 6rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    flex: 1;
+    max-width: 150px;
+    min-width: 120px;
+    margin: 1rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
