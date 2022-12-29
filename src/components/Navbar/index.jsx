e6d5624bafa2dbfa2a0c45422344a6dc8ee4cc19
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-29 10:36:23 -0300

    Responsive Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as Styled from "./styles";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
+
 function Navbar() {
   const [toogleMenu, setToggleMenu] = useState(false);
 
@@ -33,6 +34,46 @@ function Navbar() {
         <p>Sing in</p>
         <button type="button">Sign up</button>
       </Styled.Sign>
+      <Styled.Menu>
+        {toogleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toogleMenu && (
+          <Styled.MenuContainer>
+            <Styled.MenuLinks>
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#wgpt3">Whats is GPT3</a>
+              </p>
+              <p>
+                <a href="#possibility">Open Ai</a>
+              </p>
+              <p>
+                <a href="#features">Case Studies</a>
+              </p>
+              <p>
+                <a href="#blog">Library</a>
+              </p>
+              <Styled.SignLinks>
+                <p>Sing in</p>
+                <button type="button">Sign up</button>
+              </Styled.SignLinks>
+            </Styled.MenuLinks>
+          </Styled.MenuContainer>
+        )}
+      </Styled.Menu>
     </Styled.Container>
   );
 }
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -15,6 +15,26 @@ export const Container = styled.div`
     margin: 0 1rem;
     cursor: pointer;
   }
+  button {
+    padding: 0.5rem 1rem;
+    color: #fff;
+    background: #ff4820;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 25px;
+    border-radius: 5px;
+    border: 0;
+    outline: none;
+    cursor: pointer;
+  }
+
+  @media (max-width: 700px) {
+    padding: 2rem 4rem;
+  }
+
+  @media (max-width: 550px) {
+    padding: 2rem;
+  }
 `;
 
 export const Logo = styled.div`
@@ -36,6 +56,10 @@ export const Links = styled.div`
 export const LinksContainer = styled.div`
   display: flex;
   flex-direction: row;
+
+  @media (max-width: 1050px) {
+    display: none;
+  }
 `;
 
 export const Sign = styled.div`
@@ -43,18 +67,56 @@ export const Sign = styled.div`
   justify-content: flex-end;
   align-items: center;
 
-  button {
-    padding: 0.5rem 1rem;
-    color: #fff;
-    background: #ff4820;
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 25px;
-    border-radius: 5px;
-    border: 0;
-    outline: none;
-    cursor: pointer;
+  @media (max-width: 550px) {
+    display: none;
   }
 `;
 
-export const Menu = styled.div``;
+export const Menu = styled.div`
+  margin-left: 1rem;
+  display: none;
+  position: relative;
+
+  svg {
+    cursor: pointer;
+  }
+
+  @media (max-width: 1050px) {
+    display: flex;
+  }
+
+  @media (max-width: 550px) {
+    top: 20px;
+  }
+`;
+
+export const MenuContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  flex-direction: column;
+  text-align: end;
+  background: #031b34;
+  padding: 2rem;
+  position: absolute;
+  top: 40px;
+  right: 0;
+  margin-top: 1rem;
+  min-width: 210px;
+  border-radius: 5px;
+  box-shadow: 0 0 5 rgba(0, 0, 0, 0.2);
+
+  p {
+    margin: 1rem 0;
+  }
+`;
+
+export const MenuLinks = styled.div``;
+
+export const SignLinks = styled.div`
+  display: none;
+
+  @media (max-width: 550px) {
+    display: block;
+  }
+`;
