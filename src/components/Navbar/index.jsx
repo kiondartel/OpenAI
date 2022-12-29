3b3ca4f8cf0a8d99ca3a5c3450f3bb1e92815893
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2022-12-29 10:08:30 -0300

    NavBar links & assetSvg

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Styled from "./styles";
-
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 function Navbar() {
-  return <div />;
+  const [toogleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <Styled.Container>
+      <Styled.Links>
+        <Styled.Logo>
+          <img src={logo} alt="logo" />
+        </Styled.Logo>
+        <Styled.LinksContainer>
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">Whats is GPT3</a>
+          </p>
+          <p>
+            <a href="#possibility">Open Ai</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </Styled.LinksContainer>
+      </Styled.Links>
+      <Styled.Sign>
+        <p>Sing in</p>
+        <button type="button">Sign up</button>
+      </Styled.Sign>
+    </Styled.Container>
+  );
 }
 
 export default Navbar;
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,3 +1,60 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 6rem;
+  p {
+    color: #ffffff;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 25px;
+    text-transform: capitalize;
+
+    margin: 0 1rem;
+    cursor: pointer;
+  }
+`;
+
+export const Logo = styled.div`
+  margin-right: 2rem;
+
+  img {
+    width: 62px;
+    height: 16px;
+  }
+`;
+
+export const Links = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Sign = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  button {
+    padding: 0.5rem 1rem;
+    color: #fff;
+    background: #ff4820;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 25px;
+    border-radius: 5px;
+    border: 0;
+    outline: none;
+    cursor: pointer;
+  }
+`;
+
+export const Menu = styled.div``;
