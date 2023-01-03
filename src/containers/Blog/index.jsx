66ed2cd53788ae6c7ff57cfbc0fa343b382db2fc
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2023-01-03 19:25:11 -0300

    Article render

--- src/containers/Blog/index.jsx
+++ src/containers/Blog/index.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import * as Styled from "./styles";
+import Article from "../../components/Article";
 
 function Blog() {
-  return <div />;
+  return (
+    <Styled.Container>
+      <Styled.Heading>
+        <h1>
+          Muita coisa está acontecendo, <br />
+          estamos falando sobre isso.
+        </h1>
+      </Styled.Heading>
+      <Styled.BlogContainer>
+        <Styled.GroupA>
+          <Article />
+        </Styled.GroupA>
+        <Styled.GroupB>
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </Styled.GroupB>
+      </Styled.BlogContainer>
+    </Styled.Container>
+  );
 }
 
 export default Blog;
--- src/containers/Blog/styles.js
+++ src/containers/Blog/styles.js
@@ -1,3 +1,27 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 4rem 6rem;
+
+  @media (max-width: 700px) {
+    padding: 4rem;
+  }
+
+  @media (max-width: 550px) {
+    padding: 4rem 2rem;
+  }
+`;
+
+export const Heading = styled.div`
+  h1 {
+    background: linear-gradient(89.97deg, #ae67fa 1.84%, #f49867 102.67%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+`;
+
+export const BlogContainer = styled.div``;
+
+export const GroupA = styled.div``;
+export const GroupB = styled.div``;
