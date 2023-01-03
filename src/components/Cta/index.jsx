74aca760ad41f7e78922a6ea3e631f7d0498acef
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2023-01-03 19:03:32 -0300

    Cta content & styles

--- src/components/Cta/index.jsx
+++ src/components/Cta/index.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import * as Styled from "./styles";
 
 function Cta() {
-  return <div />;
+  return (
+    <Styled.Container>
+      <Styled.Content>
+        <p>Solicite acesso antencipado para começar</p>
+        <h3>Registre hoje e comece a explorar infinitas possibilidades</h3>
+      </Styled.Content>
+      <Styled.Btn>
+        <button type="button">Vamos começar</button>
+      </Styled.Btn>
+    </Styled.Container>
+  );
 }
 
 export default Cta;
--- src/components/Cta/styles.js
+++ src/components/Cta/styles.js
@@ -1,3 +1,74 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  border: 1px solid white;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  padding: 2rem;
+  margin: 4rem;
+  border-radius: 1rem;
+  background: linear-gradient(103.22deg, #ae67fa -13.86%, #f49867 99.55%);
+
+  @media (max-width: 650px) {
+    flex-direction: column;
+  }
+
+  @media (max-width: 550px) {
+    margin: 4rem 2rem;
+  }
+`;
+
+export const Content = styled.div`
+  p {
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 30px;
+    color: #000;
+  }
+
+  h3 {
+    font-size: 24px;
+    line-height: 45px;
+    color: #000;
+  }
+
+  @media (max-width: 550px) {
+    h3 {
+      line-height: 32px;
+      font-size: 18px;
+    }
+  }
+`;
+
+export const Btn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 2rem;
+  button {
+    background: #000;
+    color: #fff;
+    border-radius: 40px;
+    font-weight: bold;
+    font-size: 18px;
+    line-height: 30px;
+    padding: 0.5rem 1rem;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+
+  @media (max-width: 650px) {
+    margin: 2rem 0 0;
+  }
+
+  @media (max-width: 550px) {
+    button {
+      line-height: 28px;
+      font-size: 14px;
+    }
+  }
+`;
